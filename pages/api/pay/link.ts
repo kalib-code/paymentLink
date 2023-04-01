6a1eee0096935d11327b1edb8afee1533cf93b0b
kalibuas <Saltedasin123!@#>
2023-04-01 19:18:16 +0800

    fix webhook

--- pages/api/pay/link.ts
+++ pages/api/pay/link.ts
@@ -6,6 +6,8 @@ import axios from "axios";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
+
+
     const supabase = createServerSupabaseClient<Database>({
         req,
         res,
@@ -21,10 +23,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const method = req.method;
 
     if (method !== 'POST') {
-        return res.status(405).json({message: 'Method not allowed'})
+        return res.status(405).json({ message: 'Method not allowed' })
     }
 
-    const {amount, description , remarks} = req.body;
+    const { amount, description, remarks } = req.body;
 
     const options = {
         method: 'POST',
@@ -54,11 +56,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             type: dataRes.type,
             attributes: dataRes.attributes,
         }]).select();
-        if(error) {
-            return res.status(500).json({message: error})
+        if (error) {
+            return res.status(500).json({ message: error })
         }
         res.status(200).json(data)
-    }catch (e) {
-        res.status(500).json({message: 'Something went wrong'})
+    } catch (e) {
+        res.status(500).json({ message: 'Something went wrong' })
     }
 }
--- pages/api/pay/webhook.ts
+++ pages/api/pay/webhook.ts
@@ -4,6 +4,12 @@ import { Database } from "../../../types/supabase";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
+    const method = req.method
+    if (method !== 'POST') {
+        res.status(405).json({ error: 'Method not allowed' })
+        return
+    }
+
     const supabase = createServerSupabaseClient<Database>({
         req,
         res,
@@ -14,22 +20,41 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     // find paid payments in array 
     const paidPayments = resBody.attributes.payments.filter((payment: any) => payment.data.attributes.status === "paid");
 
-    console.log(paidPayments[0].data)
+    try {
+        const { data, error } = await supabase.from('payments').upsert([{
+            id: paidPayments[0].data.id,
+            grossAmount: paidPayments[0].data.attributes.amount,
+            netAmount: paidPayments[0].data.attributes.net_amount,
+            fees: paidPayments[0].data.attributes.fee,
+            source: paidPayments[0].data.attributes.source.type,
+            billing: paidPayments[0].data.attributes.billing,
+            reference_number: paidPayments[0].data.attributes.external_reference_number,
+            link: resBody.id,
+        }]).select();
 
-    const { data, error } = await supabase.from('payments').upsert([{
-        id: paidPayments[0].data.id,
-        grossAmount: paidPayments[0].data.attributes.amount,
-        netAmount: paidPayments[0].data.attributes.net_amount,
-        fees: paidPayments[0].data.attributes.fee,
-        source: paidPayments[0].data.attributes.source.type,
-        billing: paidPayments[0].data.attributes.billing,
-        reference_number: paidPayments[0].data.attributes.external_reference_number,
-        link: resBody.id,
-    }]).select();
+        if (error) {
+            return res.status(500).json({ message: error })
+        }
 
-    if (error) {
+        const { data: links, error: error2 } = await supabase.from('links').upsert({
+            id: resBody.id,
+            type: resBody.type,
+            attributes: resBody.attributes,
+        });
+
+        if (error2) {
+            return res.status(500).json({ message: error2 })
+        }
+        res.status(200).json({ data, links })
+
+    } catch (error) {
         return res.status(500).json({ message: error })
     }
 
-    res.status(200).json(data)
+
+
+
+
+
+
 }
